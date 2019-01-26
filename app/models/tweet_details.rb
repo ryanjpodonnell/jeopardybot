6f7aab220b7a6f6b487fe61aba368703bc4b61db
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2019-01-26 15:08:07 -0800

    Fix bug

--- app/models/tweet_details.rb
+++ app/models/tweet_details.rb
@@ -14,12 +14,12 @@ class TweetDetails
     @guess = Judge.parse_text(tweet.text.dup)
     @answer = clue.answer
     @response = Judge.check_answer(@guess.downcase, @answer)
-    @value = calculate_value
+    @value = calculate_value(clue)
   end
 
   private
 
-  def calculate_value
+  def calculate_value(clue)
     if @response == true
       return clue.value
     else
